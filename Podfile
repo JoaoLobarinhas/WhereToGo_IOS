4e27caf8ea9edb9954895583979a322079758a9d
Lobarinhas <jlobarinhasmiranda@ipvc.pt>
2019-05-29 12:12:06 +0100

    First Commit

---
+++ Podfile
@@ -0,0 +1,14 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'wheretogo' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for wheretogo
+pod 'Firebase/Core'
+pod 'Firebase/Database'
+pod 'FirebaseUI/Auth'
+pod 'Firebase/Auth'
+
+end
---
+++ wheretogo/ViewController.swift
@@ -0,0 +1,59 @@
+//
+//  ViewController.swift
+//  wheretogo
+//
+//  Created by Carlos Pinheiro on 28/05/2019.
+//  Copyright © 2019 estg.ipvc. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class ViewController: UIViewController, UITextFieldDelegate {
+
+    // MARK properties
+    
+    @IBOutlet weak var usernamelbl: UITextField!
+    @IBOutlet weak var passwordlbl: UITextField!
+    
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        scrollView.setContentOffset(CGPoint(x: 0, y: 100), animated: true)
+    }
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        scrollView.setContentOffset(CGPoint(x: 0, y: 0), animated: true)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func login(_ sender: UIButton) {
+        Auth.auth().signIn(withEmail: usernamelbl.text!, password: passwordlbl.text!) { user, error in
+            if error == nil && user != nil {
+                print("Login feito")
+            } else {
+                print("Error logging in: \(error!.localizedDescription)")
+            }
+        }
+    }
+    
+    
+}
+
+
