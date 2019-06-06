c8d076b6f6fac28d2e71854313f6b03ac0cf9ad3
Lobarinhas <jlobarinhasmiranda@ipvc.pt>
2019-06-06 15:56:24 +0100

    A aceder ao Firebase, mas query necessita fix

--- wheretogo/Tecnico/TecnicoViewController.swift
+++ wheretogo/Tecnico/TecnicoViewController.swift
@@ -11,25 +11,59 @@ import Firebase
 
 class TecnicoViewController: UIViewController,UITableViewDelegate, UITableViewDataSource {
     
-    var arrayEntities = [Service]()
+    var arrayServices = [Service]()
     
     @IBOutlet weak var tableView: UITableView!
+    
+    var ref:DatabaseReference?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let service:Service = Service(id: "1", contato: "aaa" , data: "9999", descricao: "Cena", estado: "Pendente", morada: "Viana", tecnico: "Paulo Mendes", tipo: "Reparacao", coordenadas: Coordenadas(latitude: 1.2, longitude: 2.2) )
+        ref = Database.database().reference()
         
-        arrayEntities.append(service)
+        let userID = Auth.auth().currentUser?.uid
+        print("user:"+userID!)
+        self.ref?.child("servico").queryOrdered(byChild: "tecnico").queryEqual(toValue: userID).observe(.childAdded, with: { (snapshot) in
+            
+            
+            if let dictionary = snapshot.value as? [String: AnyObject] {
+                let contato:String = dictionary["contato"] as! String
+                let data:String = dictionary["data"] as! String
+                let descricao:String = dictionary["descricao"] as! String
+                let estado:String = dictionary["estado"] as! String
+                let id:String = dictionary["id"] as! String
+                let morada:String = dictionary["morada"] as! String
+                let tecnico:String = dictionary["tecnico"] as! String
+                let tipo:String = dictionary["tipo"] as! String
+                
+                let latitude:NSNumber = dictionary["coordenadas"]!["latitude"] as! NSNumber
+                let longitude:NSNumber = dictionary["coordenadas"]!["longitude"] as! NSNumber
+                
+                print(dictionary)
+                
+                let service:Service = Service(id: id, contato: contato , data: data, descricao: descricao, estado: estado, morada: morada, tecnico: tecnico, tipo: tipo, coordenadas: Coordenadas(latitude: latitude.floatValue, longitude: longitude.floatValue ));
+                
+                self.arrayServices.append(service)
+                
+                self.tableView.reloadData()
+                
+            }
+            
+        }) { (error) in
+            print(error.localizedDescription)
+        }
+        
+        
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return arrayEntities.count
+        return arrayServices.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! TecnicoTableViewCell
-        let ec:Service = arrayEntities[indexPath.row]
+        let ec:Service = arrayServices[indexPath.row]
         cell.labelData.text = ec.data;
         cell.labelMorada.text = ec.morada;
         cell.labelEstado.text = ec.estado;
