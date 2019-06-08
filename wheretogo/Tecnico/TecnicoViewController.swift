46a467420a88e3ae48e13b284404eddce0670783
Lobarinhas <jlobarinhasmiranda@ipvc.pt>
2019-06-08 16:48:43 +0100

    Falta o Icon e melhorar as margens da cell

--- wheretogo/Tecnico/TecnicoViewController.swift
+++ wheretogo/Tecnico/TecnicoViewController.swift
@@ -8,14 +8,16 @@
 
 import UIKit
 import Firebase
+import FirebaseDatabase
+import MaterialComponents.MaterialButtons
 
 class TecnicoViewController: UIViewController,UITableViewDelegate, UITableViewDataSource {
-    
-    var arrayServices = [Service]()
     
     var services = [ServiceFirebase]()
     
     var dateF:String?
+    
+    var serviceClicker:Int = 0
     
     @IBOutlet weak var tableView: UITableView!
     
@@ -38,12 +40,13 @@ class TecnicoViewController: UIViewController,UITableViewDelegate, UITableViewDa
             
             if let dictionary = snapshot.value as? [String: AnyObject] {
                 
-                print(dictionary["tecnico"]?["id"] as! String)
-                
                 if dictionary["tecnico"]?["id"] as! String == Auxiliar.userLoged{
                 
                     let servico = ServiceFirebase(dictionary: dictionary)
-                    self.services.append(servico)
+                    
+                    if(dictionary["estado"] as! String == "Pendente" || dictionary["estado"] as! String == "Aceite"){
+                        self.services.append(servico)
+                    }
                     
                     DispatchQueue.main.async {
                         self.tableView.reloadData()
@@ -64,17 +67,26 @@ class TecnicoViewController: UIViewController,UITableViewDelegate, UITableViewDa
             
             if let dictionary = snapshot.value as? [String: AnyObject]{
                 
-                print(dictionary)
-                
                 if dictionary["tecnico"]?["id"] as! String == Auxiliar.userLoged{
                     
                     let servicoUpdated = ServiceFirebase(dictionary: dictionary)
                     
                     for i in 0..<self.services.count{
                         if self.services[i].id == servicoUpdated.id{
+                            
                             if servicoUpdated.data as! String == self.dateF as! String{
-                                self.services[i] = servicoUpdated
-                                break
+                                
+                                print("Estado update: "+servicoUpdated.estado! as! String)
+                                
+                                if(servicoUpdated.estado! as! String == "Pendente" || servicoUpdated.estado! as! String == "Aceite"){
+                                    
+                                    self.services[i] = servicoUpdated
+                                    break
+                                }
+                                else{
+                                    self.services.remove(at: i)
+                                    break
+                                }
                             }
                             else{
                                 self.services.remove(at: i)
@@ -106,6 +118,79 @@ class TecnicoViewController: UIViewController,UITableViewDelegate, UITableViewDa
         cell.labelEstado.text = ec.estado;
         cell.labelServico.text = ec.descricao;
         cell.imageEstado.image = UIImage(named: "map_blue");
+        
+        if cell.labelEstado.text == "Pendente"{
+            
+            cell.btnConcluido.setTitle("Aceitar", for: .normal)
+            cell.btnConcluido.addTarget(self, action: #selector(aceitarSerivco(sender:)), for: .touchUpInside)
+            cell.btnConcluido.tag = indexPath.row
+            
+            cell.btnCancelar.setTitle("Recusar", for: .normal)
+            cell.btnCancelar.addTarget(self, action: #selector(recusarServico(sender:)), for: .touchUpInside)
+            cell.btnCancelar.tag = indexPath.row
+            
+        }
+        else if cell.labelEstado.text == "Aceite"{
+            
+            cell.btnConcluido.setTitle("Concluir", for: .normal)
+            cell.btnConcluido.addTarget(self, action: #selector(concluirServico(sender:)), for: .touchUpInside)
+            cell.btnConcluido.tag = indexPath.row
+            
+            cell.btnCancelar.setTitle("Cancelar", for: .normal)
+            cell.btnCancelar.addTarget(self, action: #selector(cancelarServico(sender:)), for: .touchUpInside)
+            cell.btnCancelar.tag = indexPath.row
+            
+        }
+        
         return cell;
     }
+    
+    @objc func recusarServico(sender: MDCButton){
+        let serviceCanceled = services[sender.tag].id
+        
+        let alert = UIAlertController(title: "Confirmação", message: "Quer mesmo recusar o serivco?", preferredStyle: .alert)
+        let OKAction = UIAlertAction(title: "Sim", style: UIAlertActionStyle.default, handler: {
+            (_)in
+            Database.database().reference().child("servico").child(serviceCanceled!).updateChildValues(["estado":"Recusado"])
+        })
+        alert.addAction(OKAction)
+        alert.addAction(UIAlertAction(title: "Não", style: .cancel, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+        
+    }
+    
+    @objc func aceitarSerivco(sender: MDCButton){
+        let serviceAccepted = services[sender.tag].id
+        
+        Database.database().reference().child("servico").child(serviceAccepted!).updateChildValues(["estado":"Aceite"])
+    }
+    
+    @objc func cancelarServico(sender: MDCButton){
+        let serviceCanceled = services[sender.tag].id
+        
+        let alert = UIAlertController(title: "Confirmação", message: "Quer mesmo cancelar o serivco?", preferredStyle: .alert)
+        let OKAction = UIAlertAction(title: "Sim", style: UIAlertActionStyle.default, handler: {
+            (_)in
+            Database.database().reference().child("servico").child(serviceCanceled!).updateChildValues(["estado":"Cancelado"])
+        })
+        alert.addAction(OKAction)
+        alert.addAction(UIAlertAction(title: "Não", style: .cancel, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+        
+    }
+    
+    @objc func concluirServico(sender: MDCButton){
+        let serviceCanceled = services[sender.tag].id
+        
+        let alert = UIAlertController(title: "Confirmação", message: "Concluio o serviço?", preferredStyle: .alert)
+        let OKAction = UIAlertAction(title: "Sim", style: UIAlertActionStyle.default, handler: {
+            (_)in
+            Database.database().reference().child("servico").child(serviceCanceled!).updateChildValues(["estado":"Concluido"])
+        })
+        alert.addAction(OKAction)
+        alert.addAction(UIAlertAction(title: "Não", style: .cancel, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+        
+    }
+    
 }
